2445ca36a7807153229c3d2a580728227b2fe49f
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-24 23:47:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word("Hello there, and how are you?")
-    "Hello there, and how are you?".reverse_each
-  
+    "Hello there, and how are you?".each do {|x|}.reverse_each
+  end
   end
 
